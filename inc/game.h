0a5c071008fb670edc27faf6e493aad0301687d6
Jake Tockerman <jaketockerman@gmail.com>
2021-11-30 15:55:59 -0500

    initial game logic

---
+++ inc/game.h
@@ -0,0 +1,16 @@
+#ifndef _game_h_
+#define _game_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stm32f0xx.h"
+#include <stdbool.h>
+//#include <limits.h>
+
+typedef struct Node {
+    int id;
+    struct Node * next;
+} node;
+
+
+#endif
