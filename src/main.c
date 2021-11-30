de5a3155c8263d832a1635afd92d1a80314248fc
Jacob Andrew Tockerman <jtockerm@ee69lnx10.ecn.purdue.edu>
2021-11-30 15:55:59 -0500

    initial game logic

--- src/main.c
+++ src/main.c
@@ -9,34 +9,34 @@
 
 int main(void)
 {
-	nano_wait(1000000);
-	initKeypad();
-	nano_wait(1000000);
-	initLCD();
-	nano_wait(1000000);
+    nano_wait(1000000);
+    initKeypad();
+    nano_wait(1000000);
+    initLCD();
+    nano_wait(1000000);
 
-	drawimg_4bit(pj_map);
+    drawimg_4bit(pj_map);
 
-	int x, y;
-	x = 0;
-	y = 0;
+    int x, y;
+    x = 0;
+    y = 0;
 
     for(;;)
     {
-    	int8_t k = get_keypress();
-    	if (k != -1) setLED(k);
+        int8_t k = get_keypress();
+        if (k != -1) setLED(k);
 
-    	LCD_DrawPoint(x, y, ((2<<16) - 1)*(x*y)/(320*240));
-    	x = x + 1;
-    	if (x > 240)
-    	{
-    		y = y + 1;
-    		x = 0;
-    		if (y == 320)
-    			y = 0;
-    	}
+        LCD_DrawPoint(x, y, ((2<<16) - 1)*(x*y)/(320*240));
+        x = x + 1;
+        if (x > 240)
+        {
+            y = y + 1;
+            x = 0;
+            if (y == 320)
+                y = 0;
+        }
 
-		//nano_wait(1000000);
+        //nano_wait(1000000);
 
     }
 }
