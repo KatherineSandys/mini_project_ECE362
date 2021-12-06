fbdbfadbac154b0949a460ea1a7744a697e4a841
michaellangford99 <michaellangford99@gmail.com>
2021-12-06 10:58:27 -0500

    fixed timer delay

--- src/game.c
+++ src/game.c
@@ -91,7 +91,7 @@ bool read_sequence() {
     node * l = head;
     while (l != NULL && active) {
     	//time expired
-    	if (curr_counter == max_time_ms) return false;
+    	if (curr_counter >= max_time_ms) return false;
 
         for(int button = get_keypress(); button != -1; button = get_keypress()) {
             setLED(button);
@@ -114,15 +114,23 @@ void swap_mode() {
     clrAllLEDs();
 }
 
+void TIM2_IRQHandler() {
+    TIM2->SR &= ~TIM_SR_UIF;
+
+    if (curr_counter >= max_time_ms) return;
+    curr_counter += 10;
+}
+
+
 void TIM3_IRQHandler() {
     TIM3->SR &= ~TIM_SR_UIF;
 
-    if (curr_counter == max_time_ms) return;
+    if (curr_counter >= max_time_ms) return;
 
     //if at a second boundary, draw the digits.
     if (curr_counter % 100 == 0)
     {
-    	char time_remaining[5];
+    	char time_remaining[5] = {' ', ' ', ' ', ' ', '\0'};
     	int diff = (max_time_ms-curr_counter) / 100;
     	itoa(diff, time_remaining+1, 10);
     	if (diff > 10) {
@@ -133,20 +141,18 @@ void TIM3_IRQHandler() {
             time_remaining[0] = '.';
     	}
 
-    	LCD_DrawFillRectangle(320/2-10, 240/2, 320/2 + 100, 180, WHITE);
+    	//LCD_DrawFillRectangle(320/2-10, 240/2, 320/2 + 100, 180, WHITE);
 
     	LCD_DrawString(320/2 - 10, 240/2, BLACK, WHITE, time_remaining, 16, 0);
 
     }
-
-    curr_counter += 1;
 
     //must go from 0 to 240 in max_time_ms cycles (this is a 1KHz timer)
     int pixel = (320 * curr_counter) / max_time_ms;
 
     LCD_DrawFillRectangle(pixel-1, 200+1, pixel, 240-1, RED);
 
-
+    //curr_counter += 10;
 }
 
 void game_over() {
@@ -175,18 +181,18 @@ void game() {
     //Timer enables
     if (!(RCC->APB1ENR & RCC_APB1ENR_TIM2EN)) {
             RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-            TIM2->PSC = 0;
-            TIM2->ARR = 0xffffffff;
-            TIM2->CR1 |= TIM_CR1_CEN;
+            TIM2->PSC = 480-1;
+            TIM2->ARR = 1000-1;
+            TIM2->DIER |= TIM_DIER_UIE;
     }
     if (!(RCC->APB1ENR & RCC_APB1ENR_TIM3EN)) {
            RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
            TIM3->PSC = 48-1;
-           TIM3->ARR = 1000-1;
+           TIM3->ARR = 5000-1;
            TIM3->DIER |= TIM_DIER_UIE;
-           rate = (48000000.0 / TIM3->PSC + 1) / (TIM3->ARR + 1);
    }
 
+    NVIC_EnableIRQ(TIM2_IRQn);
     NVIC_EnableIRQ(TIM3_IRQn); /* (1) */
 
     LCD_Clear(WHITE);
@@ -230,6 +236,7 @@ void game() {
         //display score
         display_score_corner();
 
+        TIM2->CR1 |= TIM_CR1_CEN;
         TIM3->CR1 |= TIM_CR1_CEN;
 
         active = read_sequence();
@@ -238,6 +245,7 @@ void game() {
 
         //turn off timer
         TIM3->CR1 &= ~TIM_CR1_CEN;
+        TIM2->CR1 &= ~TIM_CR1_CEN;
 
         swap_mode();
     }
